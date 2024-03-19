4ad275d0398c2464eb7e2216e2d15d8154a736d5
BelenLarriel <larriellucia@gmail.com>
2024-03-19 10:20:37 -0300

    fondo

---
+++ src/layouts/navbar.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Outlet, Link } from 'react-router-dom';
+
+
+
+
+
+
+const NavBarExample = () => {
+  return (
+    <>
+      <Navbar className="navBg" variant="dark" expand="lg" style={{
+        
+        
+        
+        
+        backgroundColor: '#0B316E' }}>                
+
+
+
+        
+        <Container>
+
+          <Navbar.Brand as={Link} to="/">ROSHKA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+
+
+              <NavDropdown title="Cargos" id="cargos-dropdown">
+                <NavDropdown.Item as={Link} to="/cargos/agregar">Agregar </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/cargos/listar">Listar cargos</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/cargos/listarco ">Listar convocatorias</NavDropdown.Item>
+
+              </NavDropdown>
+              <NavDropdown title="Tecnologías" id="tec-dropdown">
+                <NavDropdown.Item as={Link} to="/tecnologias/agregar">Agregar</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/tecnologias/listar">Listar</NavDropdown.Item>
+              </NavDropdown>
+
+
+              <NavDropdown title="Postulante" id="post-dropdown">
+                <NavDropdown.Item as={Link} to="/postulante/listar">Listar</NavDropdown.Item> 
+              </NavDropdown>
+
+
+              <NavDropdown title="Beneficios" id="bene-dropdown">
+                <NavDropdown.Item as={Link} to="/beneficios/agregar">Agregar</NavDropdown.Item> 
+                <NavDropdown.Item as={Link} to="/beneficios/listar">Listar</NavDropdown.Item> 
+              </NavDropdown>
+
+
+              <NavDropdown title="Cumpleaños" id="cumple-dropdown">
+                <NavDropdown.Item as={Link} to="/cumpleaños/agregar">Agregar</NavDropdown.Item> 
+                <NavDropdown.Item as={Link} to="/cumpleaños/listar">Listar</NavDropdown.Item> 
+              </NavDropdown>
+ 
+              <Nav.Link as={Link} to="/editarperfil">Editar Perfil</Nav.Link>
+
+              <Nav.Link as={Link} to="/salir">Salir</Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+       
+    </>
+  );
+};
+
+
+export default NavBarExample;
