76f1522950f4e883695e00e36f21640302cdda03
Victor Parra <110433475+Victorparrd@users.noreply.github.com>
2024-03-20 13:55:20 -0300

    se agrego modal al formulario

--- src/pages/Postulante/PostulanteForm.jsx
+++ src/pages/Postulante/PostulanteForm.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
 import './PostulanteForm.css';
 import imagen from '../../imagenes/prog_backend.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap, faCode, faStar, faUser, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const PostulanteForm = () => {
+
+    const [showEstudios, setShowEstudios] = useState(false);
+    const handleCloseEstudios = () => setShowEstudios(false);
+    const handleShowEstudios = () => setShowEstudios(true);
+
+    const [showTecnologias, setShowTecnologias] = useState(false);
+    const handleCloseTecnologias = () => setShowTecnologias(false);
+    const handleShowTecnologias = () => setShowTecnologias(true);
+
+    const [showExperiencias, setShowExperiencias] = useState(false);
+    const handleCloseExperiencias = () => setShowExperiencias(false);
+    const handleShowExperiencias = () => setShowExperiencias(true);
+
+    const [showReferencias, setShowReferencias] = useState(false);
+    const handleCloseReferencias = () => setShowReferencias(false);
+    const handleShowReferencias = () => setShowReferencias(true);
+
     return (
         <div className="postulante-container">
             <h1>Título</h1>
@@ -61,12 +81,9 @@ const PostulanteForm = () => {
                 </div>
 
                 <div className="col-md-6">
-                    <label htmlFor="departamento" className="form-label">Departamento</label>
-                    <select id="departamento" className="form-select" name="departamento">
-                        <option value="cap">Capital</option>
-                        <option value="cen">Central</option>
-                        {/* Agrega más opciones según sea necesario */}
-                    </select>
+                    <label htmlFor="direccion" className="form-label">Dirección *</label>
+                    <input type="text" className="form-control" id="direccion" name="direccion"
+                        placeholder="Ingrese su dirección" />
                 </div>
 
                 <div className="col-md-6">
@@ -79,25 +96,14 @@ const PostulanteForm = () => {
                 </div>
 
                 <div className="col-md-6">
-                    <label htmlFor="direccion" className="form-label">Dirección *</label>
-                    <input type="text" className="form-control" id="direccion" name="direccion"
-                        placeholder="Ingrese su dirección" />
+                    <label htmlFor="fecha_nacimiento" className="form-label">Fecha de nacimiento *</label>
+                    <input type="date" className="form-control" id="fecha_nacimiento" name="fecha_nacimiento" />
                 </div>
 
                 <div className="col-md-6">
                     <label htmlFor="telefono" className="form-label">Teléfono *</label>
                     <input type="text" className="form-control" id="telefono" name="telefono"
                         placeholder="Ingrese su número de teléfono" />
-                </div>
-
-                <div className="col-md-6">
-                    <label htmlFor="fecha_nacimiento" className="form-label">Fecha de nacimiento *</label>
-                    <input type="date" className="form-control" id="fecha_nacimiento" name="fecha_nacimiento" />
-                </div>
-
-                <div className="col-md-6">
-                    <label htmlFor="cargar_cv" className="form-label">Cargar CV</label>
-                    <input type="file" className="form-control" id="cargar_cv" name="cargar_cv" />
                 </div>
 
                 <div className="col-md-6">
@@ -120,37 +126,268 @@ const PostulanteForm = () => {
                     </select>
                 </div>
 
-
-                <div className="col-12">
-                    <h4>Cargos *</h4>
-
+                <div className="col-md-12">
+                    <label htmlFor="cargar_cv" className="form-label">Cargar CV</label>
+                    <input type="file" className="form-control" id="cargar_cv" name="cargar_cv" />
                 </div>
 
-                <div className="col-12">
-                    <h4>Estudios</h4>
 
+
+                <div className="col-12 d-flex align-items-center">
+                    <h4 className="m-10 me-2"><FontAwesomeIcon icon={faGraduationCap} /> Estudios</h4>
+                    <Button variant="light" size="sm" onClick={handleShowEstudios}>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Button>
                 </div>
 
-                <div className="col-12">
-                    <h4>Tecnologías *</h4>
+                <>
+                    <Modal show={showEstudios} onHide={handleCloseEstudios} className="postulante-modal">
+                        <Modal.Header closeButton>
+                            <Modal.Title>Estudios</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form className='row g-3'>
+                                <Form.Group className="col-md-12" controlId="tipo_estudio">
+                                    <Form.Label>Tipo de estudio</Form.Label>
+                                    <Form.Select>
+                                        <option>Terciario</option>
+                                        <option>Secundario</option>
+                                        <option>Curso</option>
+                                        <option>Post Grado</option>
+                                        <option>Otro</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="institucion_estudio">
+                                    <Form.Label>Institución *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Nombre de la institución"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="descripcion_estudio">
+                                    <Form.Label>Carrera/Bachiller/Tema de curso *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese su carrera/bachiller/tema de curso "
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="estado_estudio">
+                                    <Form.Label>Estado</Form.Label>
+                                    <Form.Select>
+                                        <option>En curso</option>
+                                        <option>Finalizado</option>
+                                        <option>Suspendido</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="col-md-6" controlId="fecha_inicio">
+                                    <Form.Label>Fecha de inicio *</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-6" controlId="fecha_fin">
+                                    <Form.Label>Fecha de fin</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                    />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleCloseEstudios}>
+                                Cerrar
+                            </Button>
+                            <Button variant="primary" onClick={handleCloseEstudios}>
+                                Agregar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
 
+
+                <div className="col-12 d-flex align-items-center">
+                    <h4 className="m-10 me-2"><FontAwesomeIcon icon={faCode} /> Tecnologías *</h4>
+                    <Button variant="light" size="sm" onClick={handleShowTecnologias}>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Button>
                 </div>
+
+                <>
+                    <Modal show={showTecnologias} onHide={handleCloseTecnologias} className="postulante-modal">
+                        <Modal.Header closeButton>
+                            <Modal.Title>Tecnologías</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form className='row g-3'>
+                                <Form.Group className="col-md-12" controlId="tecnologia">
+                                    <Form.Label>Tecnología</Form.Label>
+                                    <Form.Select>
+                                        <option>Java</option>
+                                        <option>Python</option>
+                                        <option>Otro</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="nivel">
+                                    <Form.Label>Nivel</Form.Label>
+                                    <Form.Range />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleCloseTecnologias}>
+                                Cerrar
+                            </Button>
+                            <Button variant="primary" onClick={handleCloseTecnologias}>
+                                Agregar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
+
 
                 <div className="col-12">
                     <h4>Otras Tecnologías</h4>
                     <textarea className="form-control" id="otras_tecnologias" name="otras_tecnologias"
-                        placeholder="Si tienes alguna tecnología que no figura en la sección anterior. Escribelo aquí." rows="3"></textarea>
+                        placeholder="Si tienes alguna tecnología que no figura en la sección anterior. Escríbelo aquí." rows="3"></textarea>
                 </div>
 
-                <div className="col-12">
-                    <h4>Experiencias</h4>
-
+                <div className="col-12 d-flex align-items-center">
+                    <h4 className="m-10 me-2"><FontAwesomeIcon icon={faStar} /> Experiencias</h4>
+                    <Button variant="light" size="sm" onClick={handleShowExperiencias}>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Button>
                 </div>
 
-                <div className="col-12">
-                    <h4>Referencia Personal</h4>
+                <>
+                    <Modal show={showExperiencias} onHide={handleCloseExperiencias} className="postulante-modal">
+                        <Modal.Header closeButton>
+                            <Modal.Title>Experiencia</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form className='row g-3'>
+                                <Form.Group className="col-md-12" controlId="empresa">
+                                    <Form.Label>Empresa *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Nombre de la empresa"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="cargo">
+                                    <Form.Label>Cargo *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese su cargo"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-6" controlId="fecha_desde">
+                                    <Form.Label>Fecha de desde *</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-6" controlId="fecha_hasta">
+                                    <Form.Label>Fecha hasta</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="descripcion_experiencia">
+                                    <Form.Label>Descripción *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese una descripción"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="referencia">
+                                    <Form.Label>Nombre de la referencia</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese el nombre de su referencia"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="telefono_referencia">
+                                    <Form.Label>Teléfono de la refencia</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese el teléfono de su refencia"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="motivo_salida">
+                                    <Form.Label>Motivo de salida</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        placeholder="Ingrese el motivo de su salida"
+                                        style={{ height: '75px' }}
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="tipo_experiencia">
+                                    <Form.Label>Tipo de experiencia</Form.Label>
+                                    <Form.Select>
+                                        <option>Trabajo normal</option>
+                                        <option>Pasantia</option>
+                                    </Form.Select>
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleCloseExperiencias}>
+                                Cerrar
+                            </Button>
+                            <Button variant="primary" onClick={handleCloseExperiencias}>
+                                Agregar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
 
+
+                <div className="col-12 d-flex align-items-center">
+                    <h4 className="m-10 me-2"><FontAwesomeIcon icon={faUser} /> Referencia Personal</h4>
+                    <Button variant="light" size="sm" onClick={handleShowReferencias}>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Button>
                 </div>
+
+                <>
+                    <Modal show={showReferencias} onHide={handleCloseReferencias} className="postulante-modal">
+                        <Modal.Header closeButton>
+                            <Modal.Title>Referencia personal</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form className='row g-3'>
+                                <Form.Group className="col-md-12" controlId="nombre_relacion">
+                                    <Form.Label>Nombre *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese el nombre de su referencia"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="relacion">
+                                    <Form.Label>Relación *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese la relación con su referencia"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="col-md-12" controlId="telefono_relacion">
+                                    <Form.Label>Teléfono *</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Ingrese el número de teléfono de su relación"
+                                    />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleCloseReferencias}>
+                                Cerrar
+                            </Button>
+                            <Button variant="primary" onClick={handleCloseReferencias}>
+                                Agregar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
 
 
                 <div className="col-12 d-flex justify-content-end">
