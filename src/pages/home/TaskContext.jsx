17c69fe10b8dbc7fefce71e96887daa9e890f920
Victor Parra <110433475+Victorparrd@users.noreply.github.com>
2024-03-26 09:29:20 -0400

    cambios en el tablero 2

--- src/pages/home/TaskContext.jsx
+++ src/pages/home/TaskContext.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
 import { v4 as uuidv4 } from "uuid";
-import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const initialTasks = [
     {
         id: 0,
-        title: "Preseleccionados",
+        title: "PRESELECCIONADOS",
         tasks: [
             {
                 id: uuidv4(),
@@ -16,7 +17,7 @@ const initialTasks = [
     },
     {
         id: 1,
-        title: "Entrevista 1",
+        title: "ENTREVISTA 1",
         tasks: [
             {
                 id: uuidv4(),
@@ -27,7 +28,7 @@ const initialTasks = [
     },
     {
         id: 2,
-        title: "Entrevista 2",
+        title: "ENTREVISTA 2",
         tasks: [
             {
                 id: uuidv4(),
@@ -41,33 +42,33 @@ const initialTasks = [
 const TaskContext = createContext();
 
 const TaskProvider = ({ children }) => {
-    const [tasks, setTasks] = useState(() => {
-        const tasksFromStorage = localStorage.getItem("tasks");
-        return tasksFromStorage ? JSON.parse(tasksFromStorage) : initialTasks;
+    const [lane, setLanes] = useState(() => {
+        const tasks = localStorage.getItem("tasks");
+        return tasks ? JSON.parse(tasks) : initialTasks;
     });
 
     useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    }, [tasks]);
+        localStorage.setItem("tasks", JSON.stringify(lane));
+    }, [lane]);
 
     return (
-        <TaskContext.Provider value={{ tasks, setTasks }}>
+        <TaskContext.Provider value={{ lane, setLanes }}>
             {children}
         </TaskContext.Provider>
     );
 };
 
 const LaneSection = () => {
-    const { tasks } = useContext(TaskContext);
+    const { lane } = useContext(TaskContext);
 
     const getTaskTitle = (id) => {
         switch (id) {
             case 0:
-                return "Preseleccionados";
+                return "PRESELECCIONADOS";
             case 1:
-                return "Entrevista 1";
+                return "ENTREVISTA 1";
             case 2:
-                return "Entrevista 2";
+                return "ENTREVISTA 2";
             default:
                 return "";
         }
@@ -75,15 +76,15 @@ const LaneSection = () => {
 
     return (
         <div className="lane-container">
-            {tasks.map((lane) => (
-                <div key={lane.id} className="lane">
-                    <h2 className="lane__title">{getTaskTitle(lane.id)}</h2>
-                    <FormComponent laneId={lane.id}></FormComponent>
+            {lane.map((t) => (
+                <div key={t.id} className="lane">
+                    <h2 className="lane__title">{getTaskTitle(t.id)}</h2>
+                    <FormComponent id={t.id}></FormComponent>
                     <div className="lane__info">
-                        <span className="lane__number">({lane.tasks.length})</span>
+                        <span className="lane__number">({t.tasks.length})</span>
                     </div>
                     <div className="lane__tasks">
-                        <TasksComponent tasks={lane.tasks} type={lane.id} title={getTaskTitle(lane.id)}></TasksComponent>
+                        <TasksComponent tasks={t.tasks} type={t.id} title={getTaskTitle(t.id)}></TasksComponent>
                     </div>
                 </div>
             ))}
@@ -91,67 +92,90 @@ const LaneSection = () => {
     );
 };
 
-const FormComponent = ({ laneId }) => {
-    const [selectedState, setSelectedState] = useState(""); //agregue
-    const [estados, setEstados] = useState([]); //agregue
 
-    useEffect(() => {
-        axios.get('http://localhost:8080/thbackend/v1/estados')
-            .then(response => {
-                console.log(response.data);
-                setEstados(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching estados:', error);
-            });
-    }, []);
+const FormComponent = ({ id }) => {
+    const { lane, setLanes } = useContext(TaskContext);
+    const [name, setName] = useState("");
+
+    const createTask = (e) => {
+        e.preventDefault();
+        let currentID = parseInt(e.target.id);
+
+        if (name.trim().length > 0) {
+            setLanes(
+                lane.map((t) => {
+                    if (t.id === currentID)
+                        return {
+                            ...t,
+                            tasks: [
+                                { id: uuidv4(), name: name, timestamp: new Date().toDateString() },
+                                ...t.tasks
+                            ]
+                        };
+                    return t;
+                })
+            );
+            setName("");
+        }
+    };
 
     return (
         <form className="form">
-            <select
-                className="form__select"
-                value={selectedState}
-                onChange={(e) => setSelectedState(e.target.value)}
-            >
-                <option value="">Seleccionar estado</option>
-                {estados.map((estado) => (
-                    <option key={estado.id_estado} value={estado.estado}>{estado.estado}</option>
-                ))}
-            </select>
+            <input
+                className="form__input"
+                type="text"
+                name="task"
+                placeholder="Agregar"
+                autoComplete="off"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+
+            <button className="form__submit" type="submit" id={id} onClick={createTask}>
+                <FontAwesomeIcon icon={faPlus} />
+            </button>
+
         </form>
     );
 };
 
+
 const TasksComponent = ({ tasks, type, title }) => {
-    const { tasks: allTasks, setTasks } = useContext(TaskContext);
+    const { lane, setLanes } = useContext(TaskContext);
 
-    const changeTask = (taskId, op) => {
-        const newTasks = allTasks.map((lane) => {
-            if (lane.id === type) {
-                const otherTasks = lane.tasks.filter((task) => task.id !== taskId);
-                const currentTask = lane.tasks.find((task) => task.id === taskId);
-                const targetLane = op === '-' ? type - 1 : type + 1;
-                const targetLaneTasks = allTasks.find((lane) => lane.id === targetLane).tasks;
-                return { ...lane, tasks: otherTasks };
-            } else if (lane.id === targetLane) {
-                return { ...lane, tasks: [currentTask, ...lane.tasks] };
-            }
-            return lane;
-        });
+    const changeTask = (e, op) => {
+        const currentLane = type;
+        const currentID = e.currentTarget.value;
+        const currentTask = lane
+            .find((t) => t.id === currentLane)
+            .tasks.find((t) => t.id === currentID);
+        const otherTask = lane
+            .find((t) => t.id === currentLane)
+            .tasks.filter((t) => t.id !== currentID);
 
-        setTasks(newTasks);
+        setLanes(
+            lane.map((t) => {
+                if (t.id === currentLane) return { ...t, tasks: otherTask };
+                else if (t.id === currentLane + (op === "-" ? -1 : 1))
+                    return { ...t, tasks: [currentTask, ...t.tasks] };
+                return t;
+            })
+        );
     };
 
-    const deleteTask = (taskId) => {
-        const newTasks = allTasks.map((lane) => {
-            if (lane.id === type) {
-                const otherTasks = lane.tasks.filter((task) => task.id !== taskId);
-                return { ...lane, tasks: otherTasks };
-            }
-            return lane;
-        });
+    const deleteTask = (e) => {
+        const currentLane = type;
+        const currentID = e.currentTarget.value;
+        const otherTask = lane
+            .find((t) => t.id === currentLane)
+            .tasks.filter((t) => t.id !== currentID);
 
-        setTasks(newTasks);
+        setLanes(
+            lane.map((t) => {
+                if (t.id === currentLane) return { ...t, tasks: otherTask };
+                return t;
+            })
+        );
     };
 
     return (
@@ -159,18 +183,20 @@ const TasksComponent = ({ tasks, type, title }) => {
             {tasks.length === 0 ? (
                 <p className="task__empty">No hay nada aún...</p>
             ) : (
-                tasks.map((task) => (
-                    <article className={`task task--${title}`} key={task.id}>
-                        <p className="task__name">{task.name}</p>
+                tasks.map((t) => (
+                    <article className={`task task--${title}`} key={t.id}>
+                        <p className="task__name">{t.name}</p>
                         <div className="task__info">
-                            <span className="task__timestamp">{task.timestamp}</span>
+                            <span className="task__timestamp">{t.timestamp}</span>
                             <div className="task__buttons">
                                 {type > 0 && (
                                     <button
                                         className="task__button"
                                         type="button"
-                                        title="Previous Stage"
-                                        onClick={() => changeTask(task.id, '-')}
+                                        title="Anterior"
+                                        value={t.id}
+                                        id={type}
+                                        onClick={(e) => changeTask(e, "-")}
                                     >
                                         &#129040;
                                     </button>
@@ -179,8 +205,10 @@ const TasksComponent = ({ tasks, type, title }) => {
                                     <button
                                         className="task__button"
                                         type="button"
-                                        title="Next Stage"
-                                        onClick={() => changeTask(task.id, '+')}
+                                        title="Siguiente"
+                                        value={t.id}
+                                        id={type}
+                                        onClick={(e) => changeTask(e, "+")}
                                     >
                                         &#129042;
                                     </button>
@@ -188,8 +216,10 @@ const TasksComponent = ({ tasks, type, title }) => {
                                 <button
                                     className="task__button"
                                     type="button"
-                                    title="Delete Task"
-                                    onClick={() => deleteTask(task.id)}
+                                    title="Eliminar"
+                                    value={t.id}
+                                    id={type}
+                                    onClick={deleteTask}
                                 >
                                     &#215;
                                 </button>
