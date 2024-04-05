696d05d1338d08a041cf5e7fae1ff7ab451f681b
Victor Parra <110433475+Victorparrd@users.noreply.github.com>
2024-04-05 16:54:50 -0400

    Tablero funcionando y mejoras en Postulante

--- src/pages/home/Tablero.jsx
+++ src/pages/home/Tablero.jsx
@@ -4,21 +4,9 @@ import { useSelector } from "react-redux";
 
 function Tablero() {
     const token = useSelector(state => state.token);
-    
-    const [estados, setEstados] = useState([]);
-    const postulantes = [
-        { id: 1, nombre: 'John', apellido: 'Doe', estado_id: 1 },
-        { id: 2, nombre: 'Jane', apellido: 'Smith', estado_id: 2 },
-        { id: 3, nombre: 'Michael', apellido: 'Johnson', estado_id: 3 },
-        { id: 4, nombre: 'Emily', apellido: 'Brown', estado_id: 4 },
-        { id: 5, nombre: 'Daniel', apellido: 'Miller', estado_id: 5 },
-        { id: 6, nombre: 'Olivia', apellido: 'Davis', estado_id: 6 },
-        { id: 7, nombre: 'David', apellido: 'Garcia', estado_id: 3 },
-        { id: 8, nombre: 'Sophia', apellido: 'Rodriguez', estado_id: 5 },
-        { id: 9, nombre: 'Joseph', apellido: 'Wilson', estado_id: 6 },
-        { id: 10, nombre: 'Abigail', apellido: 'Martinez', estado_id: 3 },
-    ];
 
+    const [estados, setEstados] = useState([]);
+    const [postulantes, setPostulantes] = useState([]);
 
     useEffect(() => {
         if (token) {
@@ -29,6 +17,14 @@ function Tablero() {
             })
                 .then(response => setEstados(response.data))
                 .catch(error => console.error(error));
+
+            axios.get(`${import.meta.env.VITE_API_URL}/v1/postulante`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+                .then(response => setPostulantes(response.data))
+                .catch(error => console.error(error));
         }
     }, []);
 
@@ -38,9 +34,9 @@ function Tablero() {
                 <div key={estado.id_estado} className="columna">
                     <h2>{estado.estado}</h2>
                     {postulantes
-                        .filter(postulante => postulante.estado_id === estado.id_estado)
+                        .filter(postulante => postulante.estado.id_estado === estado.id_estado)
                         .map(postulante => (
-                            <div key={postulante.id} className="postulante">
+                            <div key={postulante.id_postulante} className="postulante">
                                 <p>{postulante.nombre} {postulante.apellido}</p>
                             </div>
                         ))}
