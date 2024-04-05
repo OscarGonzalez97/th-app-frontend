3faa4a0f514eaeca7f1462459f3087498ba97f24
BelenLarriel <larriellucia@gmail.com>
2024-04-05 09:09:00 -0400

    fix Layout token

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -2,9 +2,11 @@ import NavBar from "../Navbar"
 import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 export const Layout = ({ children }) => {
   const token = useSelector(state => state.token);
+  const navigate = useNavigate();
   useEffect(() => {
     if(!token) {
       navigate("/login")
