d852fab453db72e5ab907430111e69fc07d31770
Addaal <adalmontania@gmail.com>
2024-04-06 13:09:48 -0400

    asdads

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
+import NavBar from "../Navbar";
 
 export const Layout = ({ children }) => {
   const dispatch = useDispatch();
