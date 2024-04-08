89d09b011483ed8e3b59bebe4ce19bf190d26c8a
Lucas Acosta <lucasfabrizzio67@gmail.com>
2024-04-08 09:42:22 -0400

    se agrega estilo para los headers de las columnas

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -3,6 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Layout = ({ children }) => {
   return <div className="base">
+    <style>{`
+                .p-column-title{
+                    font-size:20px;
+                }
+            `}</style>
     <NavBar/>
     {children}
   </div>
